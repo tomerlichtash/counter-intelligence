a329662502961310efdb17dc9edadf428573efd5
Tomer Lichtash <tomer@konzepz.com>
2018-02-11 12:30:03 +0200

    added eslint

--- script.js
+++ script.js
@@ -9,17 +9,17 @@ let playState = null;
 
 // url params
 const getUrlParam = (name, url) => {
-  if (!url) url = location.href;
-  name = name.replace(/[\[]/,"\\\[").replace(/[\]]/,"\\\]");
-  var regexS = '[\\?&]' + name + '=([^&#]*)';
-  var regex = new RegExp(regexS);
-  var results = regex.exec(url);
-  return results == null ? null : results[1];
-}
+	if (!url) url = location.href;
+	name = name.replace(/[[]/,'[').replace(/[\]]/,']');
+	const regexS = '[\\?&]' + name + '=([^&#]*)';
+	const regex = new RegExp(regexS);
+	const results = regex.exec(url);
+	return results == null ? null : results[1];
+};
 
 const hasUrlParams = () => {
-  return typeof startAt == 'number' && typeof endAt == 'number' && typeof counterStyleType == 'string'
-}
+	return typeof startAt == 'number' && typeof endAt == 'number' && typeof counterStyleType == 'string';
+};
 
 const startAt = Number(getUrlParam('startAt'));
 const endAt = Number(getUrlParam('endAt'));
@@ -31,194 +31,192 @@ const offsetInput = document.querySelector('#offset');
 const hideBtn = document.querySelector('#hide');
 const showBtn = document.querySelector('#show');
 const randBtn = document.querySelector('#rand');
-const resetBtn = document.querySelector('#reset');
 const playBtn = document.querySelector('#play');
 const pauseBtn = document.querySelector('#pause');
 const counterTypeSelector = document.querySelector('#counterTypeSelector');
 const status = document.querySelector('#status');
 
 // ui events
-hideBtn.addEventListener('click', (evt) => resetGrid(false));
-showBtn.addEventListener('click', (evt) => resetGrid(true));
-randBtn.addEventListener('click', (evt) => setRandomGridValues());
-playBtn.addEventListener('click', (evt) => playAnimation());
-pauseBtn.addEventListener('click', (evt) => pauseAnimation());
+hideBtn.addEventListener('click', () => resetGrid(false));
+showBtn.addEventListener('click', () => resetGrid(true));
+randBtn.addEventListener('click', () => setRandomGridValues());
+playBtn.addEventListener('click', () => playAnimation());
+pauseBtn.addEventListener('click', () => pauseAnimation());
 
-const counterTypes = ['decimal','decimal-leading-zero','arabic-indic','armenian','upper-armenian','lower-armenian','bengali','cambodian','khmer','cjk-decimal','devanagari','georgian','gujarati','gurmukhi','hebrew','kannada','lao','malayalam','mongolian','myanmar','oriya','persian','lower-roman','upper-roman','tamil','telugu','thai','tibetan']
+const counterTypes = ['decimal','decimal-leading-zero','arabic-indic','armenian','upper-armenian','lower-armenian','bengali','cambodian','khmer','cjk-decimal','devanagari','georgian','gujarati','gurmukhi','hebrew','kannada','lao','malayalam','mongolian','myanmar','oriya','persian','lower-roman','upper-roman','tamil','telugu','thai','tibetan'];
 
 const playAnimation = () => {
-  playState = true;
-  animate(CSS_COUNTER);
-}
+	playState = true;
+	animate(CSS_COUNTER);
+};
+
 const pauseAnimation = () => {
-  playState = false;
-}
+	playState = false;
+};
 
 // list-style-type selector
 const renderTypeSelector = (defaultStyleType) => {
-  return counterTypes.map(type => {
-    const option = document.createElement('option');
-    option.value = type;
-    option.innerHTML = type.toUpperCase();
-    option.selected = type === defaultStyleType;
-    counterTypeSelector.appendChild(option)
-  });
-}
+	return counterTypes.map(type => {
+		const option = document.createElement('option');
+		option.value = type;
+		option.innerHTML = type.toUpperCase();
+		option.selected = type === defaultStyleType;
+		counterTypeSelector.appendChild(option);
+	});
+};
 
 // inputs
-countInput.addEventListener('change', (evt) => onCountChange())
-offsetInput.addEventListener('change', (evt) => onOffsetChange())
-counterTypeSelector.addEventListener('change', (evt) => setCounterType(evt.target.value))
+countInput.addEventListener('change', () => onCountChange());
+offsetInput.addEventListener('change', () => onOffsetChange());
+counterTypeSelector.addEventListener('change', (evt) => setCounterType(evt.target.value));
 
 // css counter
 document.body.appendChild(counterResetStyleTag);
 document.body.appendChild(listStyleTypeTag);
 
 const setStatus = (msg) => {
-  status.innerHTML = msg;
-}
+	status.innerHTML = msg;
+};
 
-const createNode = (index) => {
-  const el = document.createElement('label');
-  const input = document.createElement('input')
-  const span = document.createElement('span');
-  input.type = 'checkbox';
-  input.checked = true;
-  el.appendChild(input);
-  el.appendChild(span);
-  // el.setAttribute('data-debug', index);
-  return el;
-}
+const createNode = () => {
+	const el = document.createElement('label');
+	const input = document.createElement('input');
+	const span = document.createElement('span');
+	input.type = 'checkbox';
+	input.checked = true;
+	el.appendChild(input);
+	el.appendChild(span);
+	return el;
+};
 
 const addNode = (node) => {
-  nodes.push(node);
-  document.querySelector('.labels').appendChild(node);
-}
+	nodes.push(node);
+	document.querySelector('.labels').appendChild(node);
+};
 
 const getNodes = () => {
-  return Array.prototype.slice.call(nodes);
-}
+	return Array.prototype.slice.call(nodes);
+};
 
 const getInputs = () => {
-  return getNodes().map(label => label.querySelector('input[type="checkbox"]'));
-}
+	return getNodes().map(label => label.querySelector('input[type="checkbox"]'));
+};
 
 const resetGrid = (isChecked) => {
-  getInputs().map((input, index) => input.checked = isChecked || false);
-  updateStatus();
-}
+	getInputs().map((input) => input.checked = isChecked || false);
+	updateStatus();
+};
 
 const setRandomGridValues = () => {
-  const inputs = getInputs();
-  const randVals = inputs.map((d, index) => Math.floor(Math.random() * inputs.length) % 2); 
-  resetGrid();
-  randVals.map((r, index) => getInputs()[index].checked = r);
-  updateStatus();
-}
+	const inputs = getInputs();
+	const randVals = inputs.map(() => Math.floor(Math.random() * inputs.length) % 2); 
+	resetGrid();
+	randVals.map((r, index) => getInputs()[index].checked = r);
+	updateStatus();
+};
 
 const updateStatus = () => {
-  const inputs = getInputs();
-  const current = inputs.filter((input, index) => input.checked);
-  setStatus(`${current.length}/${inputs.length}`)
-}
+	const inputs = getInputs();
+	const current = inputs.filter((input) => input.checked);
+	setStatus(`${current.length}/${inputs.length}`);
+};
 
 const getOffsetValue = () => {
-  return Number(offsetInput.value);
-}
+	return Number(offsetInput.value);
+};
 
 const getCountValue = () => {
-  return Number(countInput.value);
-}
+	return Number(countInput.value);
+};
 
 const resetCSSCounter = (limit, counterName) => {
-  counterResetStyleTag.innerHTML = `body{counter-reset: ${counterName} ${limit}}`;
-}
+	counterResetStyleTag.innerHTML = `body{counter-reset: ${counterName} ${limit}}`;
+};
 
 const resetAll = (cssResetVal) => {
-  const nodes = rootNode.querySelectorAll('label');
-  Array.prototype.slice.call(nodes).map((node, index) => rootNode.removeChild(node));
-  resetCSSCounter(cssResetVal, CSS_COUNTER);
-  items = [];
-}
+	const localNodes = rootNode.querySelectorAll('label');
+	Array.prototype.slice.call(localNodes).map((node) => rootNode.removeChild(node));
+	resetCSSCounter(cssResetVal, CSS_COUNTER);
+	nodes = [];
+};
 
 const onOffsetChange = () => {
-  if (getCountValue() < getOffsetValue()) {
-    resetAll(-1);
-    return false;
-  }
-  resetAll(getOffsetValue());
-  render();
-}
+	if (getCountValue() < getOffsetValue()) {
+		resetAll(-1);
+		return false;
+	}
+	resetAll(getOffsetValue());
+	render();
+};
 
 const onCountChange = () => {
-  resetAll(getOffsetValue());
-  render();
-}
+	resetAll(getOffsetValue());
+	render();
+};
 
 const setCounterType = (counterType) => {
-  document.querySelector('body').setAttribute('data-list-type', counterType);
-}
+	document.querySelector('body').setAttribute('data-list-type', counterType);
+};
 
 const resetValues = (count, offset) => {
-  countInput.value = count;
-  offsetInput.value = offset;
-}
+	countInput.value = count;
+	offsetInput.value = offset;
+};
 
 const render = () => {
-  const nodeCount = getCountValue() - getOffsetValue();
-  if (nodeCount < 0) resetGrid(false);
-  Array(nodeCount).fill().map((n, index) => addNode(createNode(index)));
-  updateStatus();
-}
+	const nodeCount = getCountValue() - getOffsetValue();
+	if (nodeCount < 0) resetGrid(false);
+	Array(nodeCount).fill().map((n, index) => addNode(createNode(index)));
+	updateStatus();
+};
 
 const animate = () => {
-  return new Promise(resolve =>
-    setTimeout(() => {
-      const checkedNodes = getNodes().filter(node => node.querySelector('input').checked);
-      if (!checkedNodes.length || !playState) {
-        playState = false;
-        resolve({playState});
-        return playState;
-      }
+	return new Promise(resolve =>
+		setTimeout(() => {
+			const checkedNodes = getNodes().filter(node => node.querySelector('input').checked);
+			if (!checkedNodes.length || !playState) {
+				playState = false;
+				resolve({playState});
+				return playState;
+			}
 
-      const randVal = Math.random();
-      const randIndexBool = randVal > 0.5;
-      const randIndex = Math.floor(randVal * checkedNodes.length);
+			const randVal = Math.random();
+			const randIndex = Math.floor(randVal * checkedNodes.length);
       
-      const randomEl = checkedNodes[randIndex];
-      const randomElInput = randomEl.querySelector('input');
+			const randomEl = checkedNodes[randIndex];
+			randomEl.querySelector('input');
       
-      randomEl.setAttribute('data-tracked', 'tracked');
-      setTimeout(() => randomEl.removeAttribute('data-tracked'), 1450);
+			randomEl.setAttribute('data-tracked', 'tracked');
+			setTimeout(() => randomEl.removeAttribute('data-tracked'), 1450);
       
-      animate();
-      resolve();
-    }, 50)
-  )
-}
+			animate();
+			resolve();
+		}, 50)
+	);
+};
 
 const shuffleGrid = () => {
-  setRandomGridValues();
-  return new Promise(resolve => {
-    setTimeout(() => {
-      shuffleGrid();
-      resolve();
-    }, Math.random() * 5000);
-  });
-}
+	setRandomGridValues();
+	return new Promise(resolve => {
+		setTimeout(() => {
+			shuffleGrid();
+			resolve();
+		}, Math.random() * 5000);
+	});
+};
 
-const init = (cssCounterRef, counterStyleType) => {
-  resetCSSCounter(getOffsetValue(), cssCounterRef);
-  setCounterType(customCounterStyleType);
-  renderTypeSelector(customCounterStyleType);
+const init = (cssCounterRef, styleType) => {
+	resetCSSCounter(getOffsetValue(), cssCounterRef);
+	setCounterType(styleType);
+	renderTypeSelector(styleType);
   
-  if (hasUrlParams()) {
-    resetValues(endAt, startAt);
-  }
+	if (hasUrlParams()) {
+		resetValues(endAt, startAt);
+	}
 
-  render();
-  shuffleGrid();
-  playAnimation();
-}
+	render();
+	shuffleGrid();
+	playAnimation();
+};
 
-init(CSS_COUNTER, defaultCounterStyleType);
+init(CSS_COUNTER, customCounterStyleType || defaultCounterStyleType);
